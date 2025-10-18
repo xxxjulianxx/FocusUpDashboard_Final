d1ff7fb5eb2963050c92e4e272ebc9a47b457564
xxxjulianxx <julian.villa01@unicatolica.edu.co>
2025-10-18 17:32:57 -0500

    cambiar datos del front

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: { port: 5173 }
+  build: {
+    outDir: 'dist'
+  },
+  server: {
+    port: 5173
+  }
 })
